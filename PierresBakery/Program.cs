df9011bda131875fa29ec5c1f6c69491155d7415
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-17 15:38:31 -0700

    Add notes for UI objectives to Program.cs.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,10 +8,15 @@ namespace PierresBakery
   {
     public static void Main()
     {
-
-
-
+      // user should receive prompt with welcome message
+      // user should receive cost for bread and pastries with specials included
+      // ask user if they'd like to order
+      // if no, give user nice message
+      // if yes, prompt user how many loaves of bread and how many pastries - indicate if they do not want any x to enter 0
       
+
+      // return the cost of the order
+      // return a summary of their order
     }
   }
 }
